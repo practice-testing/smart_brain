6e2ed7a663956864dea404a2dbe0071f5c9db57f
a.kalai <kalaiarasan.a@purpleslate.in>
2020-04-18 18:26:21 +0530

    displaying register errors

--- controllers/register.js
+++ controllers/register.js
@@ -23,7 +23,10 @@ const handleRegister = (req, res, db, bcrypt) => {
       })
       .then(trx.commit)
       .catch(trx.rollback);
-  }).catch((err) => res.status(400).json("unable to register"));
+  }).catch((err) => {
+    console.error(err);
+    res.status(400).json("unable to register");
+  });
 };
 
 module.exports = {
